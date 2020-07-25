6a8bebd6ca54cb6682505a7376a2311ab2c9c483
mario <esmp_256@hotmail.com>
2020-07-25 02:48:38 -0500

    ucCategoria

---
+++ ecommerce.WebASP/Logica/LogicaCategoria.cs
@@ -0,0 +1,100 @@
+﻿using ecommerce.WebASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ecommerce.WebASP.Logica
+{
+    public class LogicaCategoria
+    {
+        private static BDDCORDICARRITOEntities db = new BDDCORDICARRITOEntities();
+
+        public static async Task<List<TBL_CATEGORIA>> getAllCategory()
+        {
+            try
+            {
+                return await db.TBL_CATEGORIA.Where(data => data.CAT_STATUS == "A"
+                ).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar categoria");
+            }
+        }
+
+        public static async Task<TBL_CATEGORIA> getCategoryXId(int codigo)
+        {
+            try
+            {
+                return await db.TBL_CATEGORIA.Where(data => data.CAT_STATUS == "A"
+                && data.CAT_ID.Equals(codigo)
+                ).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar categoria");
+            }
+        }
+
+        public static async Task<bool> saveProduct(TBL_CATEGORIA _infoCategoria)
+        {
+            try
+            {
+                //auditoria basica
+                bool resultado = false;
+                _infoCategoria.CAT_STATUS = "A";
+                _infoCategoria.CAT_FECHACREACION = DateTime.Now;
+                db.TBL_CATEGORIA.Add(_infoCategoria);
+
+                //Actualizar Contexto Datos
+                await db.SaveChangesAsync();
+                resultado = true;
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar categoria");
+            }
+        }
+
+        public static async Task<bool> updateProduct(TBL_CATEGORIA _infoCategoria)
+        {
+            try
+            {
+                bool resultado = false;
+                _infoCategoria.CAT_FECHACREACION = DateTime.Now;
+
+                //Actualizar Contexto Datos
+                await db.SaveChangesAsync();
+                resultado = true;
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar categoria");
+            }
+        }
+
+        public static async Task<bool> deleteProduct(TBL_CATEGORIA _infoCategoria)
+        {
+            try
+            {
+                bool resultado = false;
+                _infoCategoria.CAT_FECHACREACION = DateTime.Now;
+                _infoCategoria.CAT_STATUS = "I";
+
+                //Actualizar Contexto Datos
+                await db.SaveChangesAsync();
+                resultado = true;
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar categoria");
+            }
+        }
+    }
+}
---
+++ ecommerce.WebASP/Logica/LogicaProducto.cs
@@ -0,0 +1,114 @@
+﻿using ecommerce.WebASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ecommerce.WebASP.Logica
+{
+    public class LogicaProducto
+    {
+        private static BDDCORDICARRITOEntities db = new BDDCORDICARRITOEntities();
+
+        public static async Task<List<TBL_PRODUCTO>> getAllProduct()
+        {
+            try
+            {
+                return await db.TBL_PRODUCTO.Where(data => data.PRO_STATUS == "A"
+                ).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar el producto");
+            }
+        }
+
+        public static async Task<TBL_PRODUCTO> getProductXId(int codigo)
+        {
+            try
+            {
+                return await db.TBL_PRODUCTO.Where(data => data.PRO_STATUS == "A"
+                && data.PRO_ID.Equals(codigo)
+                ).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar el producto");
+            }
+        }
+
+        public static async Task<TBL_PRODUCTO> getProductXCode(string codigo)
+        {
+            try
+            {
+                return await db.TBL_PRODUCTO.Where(data => data.PRO_STATUS == "A"
+                && data.PRO_CODIGO.Equals(codigo)
+                ).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar el producto");
+            }
+        }
+
+        public static async Task<bool> saveProduct(TBL_PRODUCTO _infoPRODUCTO)
+        {
+            try
+            {
+                //auditoria basica
+                bool resultado = false;
+                _infoPRODUCTO.PRO_STATUS = "A";
+                _infoPRODUCTO.PRO_FECHACREACION = DateTime.Now;
+                db.TBL_PRODUCTO.Add(_infoPRODUCTO);
+
+                //Actualizar Contexto Datos
+                await db.SaveChangesAsync();
+                resultado = true;
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar el producto");
+            }
+        }
+
+        public static async Task<bool> updateProduct(TBL_PRODUCTO _infoPRODUCTO)
+        {
+            try
+            {
+                bool resultado = false;
+                _infoPRODUCTO.PRO_FECHACREACION = DateTime.Now;
+
+                //Actualizar Contexto Datos
+                await db.SaveChangesAsync();
+                resultado = true;
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar el producto");
+            }
+        }
+
+        public static async Task<bool> deleteProduct(TBL_PRODUCTO _infoPRODUCTO)
+        {
+            try
+            {
+                bool resultado = false;
+                _infoPRODUCTO.PRO_FECHACREACION = DateTime.Now;
+                _infoPRODUCTO.PRO_STATUS = "I";
+
+                //Actualizar Contexto Datos
+                await db.SaveChangesAsync();
+                resultado = true;
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al consultar el producto");
+            }
+        }
+    }
+}
